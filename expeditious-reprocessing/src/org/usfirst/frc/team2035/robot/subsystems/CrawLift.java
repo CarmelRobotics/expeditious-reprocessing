50871d982f39f186c34936c4d5d60ff40f07cc5e
alambert14 <Robotics@10.20.35.70>
2015-01-17 17:33:13 -0800

    Added a CrawLift subsystem with methods to lift the craw up and down.

---
+++ expeditious-reprocessing/src/org/usfirst/frc/team2035/robot/subsystems/CrawLift.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team2035.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+import org.usfirst.frc.team2035.robot.RobotMap;
+import edu.wpi.first.wpilibj.Talon;
+
+public class CrawLift extends Subsystem {
+
+	
+	private DigitalInput limitSwitchBottom;
+	private DigitalInput limitSwitchTop;
+	private Talon crawMotor;
+	
+	public CrawLift() {
+		limitSwitchBottom = new DigitalInput(RobotMap.DIG_IN_CHANNEL1);
+		limitSwitchTop = new DigitalInput(RobotMap.DIG_IN_CHANNEL2);
+		crawMotor = new Talon(RobotMap.CRAW_MOTOR);
+	}
+	
+	
+	protected void initDefaultCommand()
+	{
+		
+	}
+	
+	
+	public void liftUp() 
+	{
+		if(limitSwitchTop.get() == true)
+		{
+			crawMotor.set(-RobotMap.CRAW_SPEED_UP);
+		}
+		else if(limitSwitchTop.get() == false)
+		{
+			crawMotor.set(0.00);
+		}
+		
+	}
+	
+	public void liftDown()
+	{
+		if(limitSwitchBottom.get() == true)
+		{
+			crawMotor.set(-RobotMap.CRAW_SPEED_DOWN);
+		}
+		else if(limitSwitchBottom.get() == false)
+		{
+			crawMotor.set(0.00);
+		}
+	}
+}
